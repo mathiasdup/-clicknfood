39c058aa864fbbcedd7173fd238ddc038701dfc2
Dup <matdup73@gmail.com>
2020-02-25 10:01:09 +0100

    SEED INTEGRATION

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+User.destroy_all
+Meal.destroy_all
+Order.destroy_all
+
+  azerty = User.new(
+    username: "azerty",
+    email: "azerty@test.com",
+    address: "Lyon",
+    password: "azerty"
+  )
+  azerty.save!
+  qwerty = User.new(
+    username: "qwerty",
+    email: "qwerty@test.com",
+    address: "Bristol",
+    password: "qwerty",
+  )
+  qwerty.save!
+puts 'Creating meals...'
+  burritos = Meal.new(
+    name: "burritos",
+    description: "con carne",
+    price: 40,
+    location: "lyon",
+    quantity: 4,
+    category: "Mexican",
+    user: azerty
+  )
+  burritos.save!
+ sushi = Meal.new(
+    name: "sushi",
+    description: "with salmon",
+    price: 80,
+    location: "lyon",
+    category: "Japanese",
+    quantity: 9,
+    user: azerty
+  )
+  sushi.save!
+  puts 'Creating order...'
+  order_sushi_qwerty = Order.new(
+    user: qwerty,
+    meal: sushi
+  )
+  order_sushi_qwerty.save!
+puts 'Finished!'
