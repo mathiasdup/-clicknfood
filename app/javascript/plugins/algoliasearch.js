b425bc780b2e3487bbe715e8e0d2733e4c9bd8c0
marie <marie.blanchet.pro@gmail.com>
2020-02-28 14:00:39 +0100

    Multiple checkbox search

--- app/javascript/plugins/algoliasearch.js
+++ app/javascript/plugins/algoliasearch.js
@@ -1,4 +1,5 @@
-import { displaySearch } from '../plugins/algoliadisplay'
+// import { displaySearch } from '../plugins/algoliadisplay'
+import { displayCat } from '../plugins/algoliadisplay'
 
 const algoliasearch = require("algoliasearch");
 
@@ -6,30 +7,64 @@ const client = algoliasearch(window.algolia_application_id, algolia_search_only_
 const index = client.initIndex("Meal");
 const cardsContainer = document.querySelector('.container-cards');
 
+
+const updateCards = (query) => {
+  index
+  .search(query, {
+  'optionalWords': query
+})
+  .then(({ hits }) => {
+    displayCat(hits)
+  });
+}
+
 const initAlgoliaSearch = () => {
 
 // console.log("je suis dans initAlgoliaSearch");
 
-const form = document.querySelector('.algolia-form');
+// const form = document.querySelector('.algolia-form');
 // console.log(form);
-form.addEventListener("keyup", (event) => {
-  event.preventDefault();
-  // cardsContainer.innerHTML = '';
-  const input = document.querySelector('.search-input');
-  // console.log(input.value);
-  const query = input.value;
+// form.addEventListener("keyup", (event) => {
+//   event.preventDefault();
+//   // cardsContainer.innerHTML = '';
+//   const input = document.querySelector('.search-input');
+//   // console.log(input.value);
+//   const query = input.value;
 
-index
-  .search(query)
-  .then(({ hits }) => {
-    // console.log(hits);
-    displaySearch(hits);
-  })
-  .catch(err => {
-    console.log(err);
+// index
+//   .search(query)
+//   .then(({ hits }) => {
+//     // console.log(hits);
+//     displaySearch(hits);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// });
+
+const checkboxs = document.querySelectorAll("input[type='checkbox']");
+
+const getCategories = () => {
+  const arr = []
+  checkboxs.forEach((c) => {
+    console.log(c.value)
+    if(c.checked) {
+      arr.push(c.value)
+    }
   });
+  return arr;
+}
 
+checkboxs.forEach((c) => {
+  c.addEventListener("change", (event) => {
+    event.preventDefault();
+    const values = getCategories()
+    updateCards(values.join(' '))
+  });
 });
+
+
 
 
 
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,13 +1,15 @@
 class Meal < ApplicationRecord
   include AlgoliaSearch
 
+  CATEGORIES = %w(french chinese mexican japanese italian indian lebanese viet)
+
   has_many :orders
   belongs_to :user
   has_one_attached :photo
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :category, presence: true, inclusion: { in: %w(french chinese mexican japanese italian indian lebanese viet) }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :quantity, presence: true, inclusion: { in: 1..10 }
   validates :location, presence: true
   validates :price, presence: true
@@ -15,6 +17,7 @@ class Meal < ApplicationRecord
 
   algoliasearch do
     attributes :location, :category, :name
+    attributesForFaceting [:category]
   end
 
 end
