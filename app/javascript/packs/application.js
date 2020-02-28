0ac90288b17b835af09ffcdd9fa70ae5772ae627
Dup <matdup73@gmail.com>
2020-02-28 14:02:34 +0100

    divers bug mefaites pas chier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
-import { croque } from '../plugins/croque';
 import { initAlgoliaSearch } from '../plugins/algoliasearch'
 
 
-croque();
 initAlgoliaSearch();
-
-
-// Pour Test - à supprimer
-console.log(window.algolia_application_id);
-console.log(window.algolia_search_only_api_key);
-//
